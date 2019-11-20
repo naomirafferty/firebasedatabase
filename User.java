20cc4a2cbd67c3a6c46e96575c574024f7e99ca3
naomirafferty <56118682+naomirafferty@users.noreply.github.com>
2019-11-20 11:40:26 +0000

    User.java

---
+++ User.java
@@ -0,0 +1,68 @@
+package com.example.week7adv;
+
+public class User {
+    String userId;
+    String userName;
+    String userEmail;
+    String userPassword;
+    String userPhoneNo;
+    String userAddress;
+    String userCity;
+    String userPostcode;
+    String userDOB;
+
+    public User() {
+
+    }
+
+    public User(String userId, String userName, String userEmail,
+                String userPassword, String userPhoneNo, String userAddress, String userCity,
+                String userPostcode, String userDOB) {
+
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userPhoneNo = userPhoneNo;
+        this.userAddress = userAddress;
+        this.userCity = userCity;
+        this.userPostcode = userPostcode;
+        this.userDOB = userDOB;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserPhoneNo() {
+        return userPhoneNo;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public String getUserCity() {
+        return userCity;
+    }
+
+    public String getUserPostcode() {
+        return userPostcode;
+    }
+
+    public String getUserDOB() {
+        return userDOB;
+    }
+}
